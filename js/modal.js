fe5ba913e58e756df33042b809888cec6bf0e47c
VolodymyrProtsiv <vova3582@gmail.com>
2021-12-29 16:14:24 +0200

    up

--- js/modal.js
+++ js/modal.js
@@ -9,6 +9,6 @@
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('backdrop__hidden');
+    refs.modal.classList.toggle('is-hidden');
   }
 })();
